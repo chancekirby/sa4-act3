4a01c5222789e2957f6d589b82df6cab1f1aef7a
Chance Kirby <chance14@comcast.net>
2024-02-06 11:46:31 -0600

    Added a guess limit

--- guess_number.py
+++ guess_number.py
@@ -2,10 +2,20 @@ number = 10
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
+guess_count = 1
+guess_limit = 5
 
 while True:
     print(f"Sorry! That's incorrect.")
+
+    if guess_count >= guess_limit:
+        print("Sorry. Out of guesses.")
+        print(f"The number was {number}")
+        break
+
+    print(f'You have {guess_limit - guess_count} guesses left.')
     guess = input("Guess again (enter q to exit): ")
+    guess_count += 1
 
     if guess == "q":
         print(f"The number was {number}") 
@@ -14,5 +24,7 @@ while True:
     if int(guess) == number:
         print("That is correct!")
         break
+    
+    
 
 
