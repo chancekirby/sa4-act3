6597a3eae24363747ddd9975b07c3d90b5cee814
Chance Kirby <chance14@comcast.net>
2024-02-06 11:32:28 -0600

    Added hints to indicate if the user needs to guess higher or lower

--- guess_number.py
+++ guess_number.py
@@ -5,6 +5,10 @@ guess = int(input("What number am I thinking of? "))
 
 while True:
     print(f"Sorry! That's incorrect.")
+
+    if int(guess) < number: print("Guess higher...")
+    else: print("Guess lower...")
+
     guess = input("Guess again (enter q to exit): ")
 
     if guess == "q":
@@ -14,5 +18,4 @@ while True:
     if int(guess) == number:
         print("That is correct!")
         break
-
 
